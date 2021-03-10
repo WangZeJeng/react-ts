b61c222c6cc14128541110d0754c271d252aef64
WangZJ <email@example.com>
2021-03-10 16:49:36 +0800

    add components

---
+++ src/components/home1.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home1 = (): JSX.Element => {
+  return <h1>Home1 is Working</h1>;
+};
+
+export default Home1;
---
+++ src/components/home2.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home2 = (): JSX.Element => {
+  return <h1>Home2 is Working</h1>;
+};
+
+export default Home2;
